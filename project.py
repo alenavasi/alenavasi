87607f8ade9dce5f90455ad5f71d18794d6b2cf5
theoberlier <theo.berlier@epfl.ch>
2022-11-14 17:49:03 +0100

    t project.py

--- project.py
+++ project.py
@@ -2,9 +2,10 @@ import math
 import matplotlib.pyplot as plt
 # alenaaaaaa
 increment = 0.2         # =! ajout au compteur #
+yo = 0
 
 def fonction(x):
-    y = -x**2
+    y = math.cos(x) + yo
     return y
 
 def derivee_f():
@@ -40,16 +41,89 @@ while compteur < 10:               # liste pour la fonction
     compteur += 0.1
 
 
+# Somme des forces = P + Ff = ma #
+
+def theta_f(der):
+    t = []
+    for i in range(0, len(der)):
+        angle = math.atan(der[i])
+        t.append(angle)
+    return t
+
+
+
+def time_f():
+    t = []
+    compteur = 0
+    while compteur<10:
+        t.append(compteur)
+        compteur += 0.1 
+    return t
+
+
+def vitesse_f1(m, k, t, a):
+    v = []
+    for i in range(0,len(a)):
+        vi = (math.sin(a[i])*g*m)/k + math.exp(-(k/m)*t[i]) 
+        v.append(vi)
+    return v
+
+def vitesse_f2(px, py):
+    v = []
+    for i in range(0,len(px)):
+        vi = math.sqrt(px[i]**2 + py[i]**2)/0.1
+        v.append(vi)
+    return v
+
+
+def position_sans_normale(m, k, t, a):
+    p = []
+    for i in range(0, len(a)):
+        r = -(m/k)*math.exp(-(k/m)*t[i]) + (math.sin(a[i])*g*m*t[i])/k 
+        p.append(r)
+    return p
+
+
+def position_vraie_x(pos,a):
+    x = []
+    for i in range(0,len(pos)):
+        x.append(-math.cos(a[i])*pos[i])
+    return x
+
+
+def position_vraie_y(pos,a):
+    y = []
+    for i in range(0,len(pos)):
+        y.append(-math.sin(a[i])*pos[i] + yo)
+    return y
+
+
+#calcul
+coef_frot = 10
+masse = 1
+g = 9.81
+theta = theta_f(derivee)        #angle en radiant#
+time = time_f()
+#vitesse = vitesse_f1(masse, coef_frot, time, theta)
+position_fausse = position_sans_normale(masse, coef_frot, time, theta)
+x_traj = position_vraie_x(position_fausse, theta)
+y_traj = position_vraie_y(position_fausse, theta)
+vitesse = vitesse_f2(x_traj, y_traj)
+print(vitesse)
+
+
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.plot(x, y, label='Fonction')
 #plt.plot(x, derivee, label='Derivée')
-plt.plot(x, tangente, label=f'Tangente en x={position/10}')
+#plt.plot(x, tangente, label=f'Tangente en x={position/10}')
+plt.plot(x, vitesse, label='Vitesse')
+plt.plot(x_traj, y_traj, label='Trajectoire')
 
-plt.annotate('Position = (%.1f ; %.1f)'%(x[position],y[position]), 
-             xy = (x[position], y[position]), 
-             xytext =(x[position], y[position]-derivee[position]/2), 
-             arrowprops=dict(arrowstyle = "->", facecolor='black'))
+#plt.annotate('Position = (%.1f ; %.1f)'%(x[position],y[position]), 
+#             xy = (x[position], y[position]), 
+#             xytext =(x[position], y[position]-derivee[position]/2), 
+#             arrowprops=dict(arrowstyle = "->", facecolor='black'))
 #plt.annotate('Derivée = %.1f'%(derivee[position]), 
 #             xy = (x[position], derivee[position]), 
 #             xytext =(x[position], derivee[position]-derivee[position]/2), 
